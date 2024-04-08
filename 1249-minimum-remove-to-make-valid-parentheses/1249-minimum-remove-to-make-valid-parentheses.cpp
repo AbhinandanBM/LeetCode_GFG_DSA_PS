4ff9266c1aff665bed96b3255b3dc0bb235b655d
Abhinandan B M <bmabhi13@gmail.com>
2024-04-08 09:56:34 +0530

    Time: 27 ms (17.71%), Space: 13.4 MB (32.48%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,34 +1,52 @@
 class Solution {
 public:
+//     Approach-2
+//     Iterate left to right-Eliminate the extra close brackets
+//     Iterate right to left-Eliminate the extra open brackets
     string minRemoveToMakeValid(string s) {
         int n=s.length();
-        stack<int> st;
         
-        unordered_set<int> remove_idx;
         
+        //Iterate left to right -> Eliminate the extra close brackets
+        
+        string result="";
+        int open=0;
         for(int i=0;i<n;i++){
-            if(s[i]=='(')
-                st.push(i);
-            else if(s[i]==')'){
-                if(st.empty())
-                    remove_idx.insert(i);
-                else
-                    st.pop();
+            if(s[i]>='a' && s[i]<='z')
+                result.push_back(s[i]);
+            else if(s[i]=='('){
+                open++;
+                result.push_back(s[i]);
+            }
+            else{
+                if(open>0){
+                    open--;
+                    result.push_back(s[i]);
+                }
             }
         }
         
-//         for only open brackets
-        while(!st.empty()){
-            remove_idx.insert(st.top());
-            st.pop();
+        //Iterate right to left -> Eliminate the extra open brackets
+        
+        string final_result="";
+        int close=0;
+        for(int i=result.length()-1;i>=0;i--){
+            if(result[i]>='a' && result[i]<='z')
+                final_result.push_back(result[i]);
+            else if(result[i]==')'){
+                close++;
+                final_result.push_back(result[i]);
+            }
+            else{
+                if(close>0){
+                    close--;
+                    final_result.push_back(result[i]);
+                }
+            }
         }
         
-        string result="";
-        for(int i=0;i<n;i++){
-            if(remove_idx.find(i)==remove_idx.end())
-                result.push_back(s[i]);
-        }
+        reverse(begin(final_result),end(final_result));
+        return final_result;
         
-        return result;
     }
 };
